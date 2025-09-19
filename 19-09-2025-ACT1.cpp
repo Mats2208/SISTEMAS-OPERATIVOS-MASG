be2ac94161167449ef71ebd9cbfb67765d138807
Mateo2208 <a2024111556@estudiantes.upsa.edu.bo>
2025-09-19 14:30:42 -0400

    19-09-2025 -act 1

---
+++ 19-09-2025-ACT1.cpp
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    int fd[2];
+    pid_t pid;
+    char mensaje[] = "Hola desde el padre!";
+    char buffer[100];
+
+    pipe(fd);    
+    pid = fork();
+
+    if (pid > 0) { 
+        close(fd[0]);
+        write(fd[1], mensaje, strlen(mensaje) + 1);
+        close(fd[1]);
+    } else {
+        close(fd[1]);
+        read(fd[0], buffer, sizeof(buffer));
+        printf("Hijo recibió: %s\n", buffer);
+        close(fd[0]);
+    }
+    return 0;
+}
